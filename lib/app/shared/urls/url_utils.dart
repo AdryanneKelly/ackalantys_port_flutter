a066600c9c4be96411ef2de127e404b37a101f0e
Adryanne Kelly <adryannekelly123@gmail.com>
2024-05-05 19:58:35 -0300

    removing bot url

--- lib/app/shared/urls/url_utils.dart
+++ lib/app/shared/urls/url_utils.dart
@@ -6,8 +6,7 @@ class UrlUtils {
   static const repositoriesGithub = 'https://github.com/AdryanneKelly?tab=repositories';
   static const email = 'adryannekelly.dev@gmail.com';
   static const resume = 'https://drive.google.com/file/d/17UwTehRR-lXgVN-JaRE-mejP63UBMrZq/view?usp=drive_link';
-  static const botUrl =
-      "https://discord.com/api/webhooks/1234473417139294309/gciaJy7MNUF74AN5gmLIq3fpM8Y1hBd7fPEN5OamPbh9dn68iwi65r4XjNHGRjZ0bRnv";
+
   static const project1Url = 'https://github.com/AdryanneKelly/dev-links';
   static const project2Url = 'https://orbetelecom.com.br/';
   static const project3Url = 'https://github.com/AdryanneKelly/orders_flutter';
